0974715c171504e344a5ff93575166efd5c86e41
neofytr <imagination12344321.com@gmail.com>
2025-04-03 16:22:55 +0530

    setup termination signal

--- main.go
+++ main.go
@@ -1,23 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 const serverPort = "6969"
 
 func handleConnection(conn net.Conn) {
+	defer conn.Close()
 	message := "hi!\n"
+
 	num, err := conn.Write([]byte("hi!\n"))
 	if err != nil {
 		log.Printf("ERROR: could not write message to %s\n", conn.RemoteAddr())
 	}
 
 	if num <= len(message) {
-		log.Printf("ERROR: could not write the entire message '%s' to %s\n", conn.RemoteAddr())
+		log.Printf("ERROR: could not write the entire message '%s' to %s\n", message, conn.RemoteAddr())
 	}
-	conn.Close()
 	log.Printf("INFO: closed connection from: %s\n", conn.RemoteAddr())
 }
 
@@ -27,15 +32,28 @@ func main() {
 		log.Fatalf("ERROR: could not listen to the port %s: %s\n", serverPort, err)
 	}
 
+	defer func() {
+		fmt.Println("Shutting down the TCP server on port:", serverPort)
+		handler.Close()
+	}()
+
 	log.Printf("INFO: started server on port: %s\n", serverPort)
 
-	for {
-		conn, err := handler.Accept()
-		if err != nil {
-			log.Println("ERROR: couldn't accept a connection:", err)
-			continue
+	// setup a signal handler for termination signal (Ctrl+C)
+	stop := make(chan os.Signal, 1)
+	signal.Notify(stop, os.Interrupt, syscall.SIGTERM)
+
+	go func() {
+		for {
+			conn, err := handler.Accept()
+			if err != nil {
+				log.Println("ERROR: couldn't accept a connection:", err)
+				continue
+			}
+			log.Println("INFO: accepted connection from:", conn.RemoteAddr())
+			go handleConnection(conn)
 		}
-		log.Println("INFO: accepted connection from:", conn.RemoteAddr())
-		go handleConnection(conn)
-	}
+	}()
+
+	<-stop // wait for termination signal
 }
