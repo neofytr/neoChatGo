f6be4b9efbf049873f17f9fdd49db2c3dbbd6451
neofytr <imagination12344321.com@gmail.com>
2025-04-04 22:20:50 +0530

    new

--- main.go
+++ main.go
@@ -12,15 +12,22 @@ const serverPort = "6969"
 
 func handleConnection(connection net.Conn) {
 	defer connection.Close()
+
+	message := "hi!\n"
+	num, err := connection.Write([]byte(message))
+	if err != nil {
+		log.Printf("ERROR: error writing message to the client IP:Port %s\n", connection.RemoteAddr())
+		if num < len(message) && num > 0 {
+			log.Printf("ERROR: couldn't write the entire message to the client IP:Port %s; wrote only %s\n", connection.RemoteAddr(), message[:num])
+		}
+		return
+	}
 }
 
 func main() {
-	// create Ctrl+C (SIGINT) signal handler to gracefully shut down the server upon termination
+	// Create Ctrl+C (SIGINT) signal handler
 	termSignal := make(chan os.Signal, 1)
 	signal.Notify(termSignal, syscall.SIGINT)
-
-	// channel to notify the thread accepting connections that server has been shut down
-	acceptNotify := make(chan bool)
 
 	listener, err := net.Listen("tcp", ":"+serverPort)
 	if err != nil {
@@ -36,34 +43,34 @@ func main() {
 	}()
 
 	log.Printf("INFO: Chat Server started on port %s\n", serverPort)
+
+	// channel to stop accepting new connections
+	stop := make(chan struct{})
+
 	go func() {
 		for {
-			conn, err := listener.Accept()
-			if err != nil {
-				switch {
-				case <-acceptNotify:
-					{
-						log.Printf("INFO: server received SIGINT\n")
-						return
-					}
-				default:
-					{
-						log.Printf("ERROR: couldn't accept a connection: %s", err.Error())
-						continue
-					}
+			select {
+			case <-stop:
+				log.Println("INFO: Server shutting down, stopping accept loop")
+				return
+			default:
+				conn, err := listener.Accept()
+				if err != nil {
+					log.Printf("ERROR: couldn't accept a connection: %s", err.Error())
+					continue
 				}
 
+				log.Printf("INFO: accepted connection from IP:Port -> %s\n", conn.RemoteAddr())
+
+				go handleConnection(conn)
 			}
-
-			log.Printf("INFO: accepted connection from IP:Port -> %s\n", conn.RemoteAddr())
-
-			// handle each connection in a new goroutine
-			go handleConnection(conn)
 		}
 	}()
 
-	<-termSignal // wait for the termination signal
+	<-termSignal // wait for termination signal
 
-	// termination signal is now received, signal the acceptor routine to exit
-	close(acceptNotify) // a closed channel is always ready
+	// signal the acceptor routine to stop
+	close(stop)
+
+	log.Println("INFO: Server shutting down gracefully")
 }
